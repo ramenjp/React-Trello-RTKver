e3fe6e59a3d6c4abe69c3523b97782357574612e
ramenjp <takagiyousuke5@gmail.com>
2022-02-17 17:58:44 +0900

    ボード作成まで

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,14 @@
-import path from 'path'
+const path = require('path');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+    cssModules: true,
+});
 
 module.exports = {
   webpack(config) {
     config.resolve.alias = {
+       ...config.resolve.alias,
       '@': path.resolve(__dirname, './')
     }
     return config
